45efe42a748bb9f2406488784e6eed8055f78871
emil-1996 <emil@dworniczak.pl>
2020-09-11 09:12:50 +0200

    do poprawienia addTasksFunction

--- myapp/db.js
+++ myapp/db.js
@@ -25,6 +25,25 @@ async function getDatabasesList() {
     await call(listDatabases);
 }
 
+async function addTasksFunction(client) {
+    client.db.collection('tasks', function (err, collection) {
+        collection.insert({id: 1, firstName: 'Steve', lastName: 'Jobs'});
+        collection.insert({id: 2, firstName: 'Bill', lastName: 'Gates'});
+        collection.insert({id: 3, firstName: 'James', lastName: 'Bond'});
+        client.db.collection('tasks').count(function (err, count) {
+            if (err)
+                throw err;
+
+            console.log('Total Rows: ' + count);
+        });
+    });
+}
+
+async function adTask() {
+    await call(addTasksFunction);
+}
+
 module.exports = {
-    getDatabasesList: getDatabasesList
+    getDatabasesList: getDatabasesList,
+    adTask: adTask
 }
