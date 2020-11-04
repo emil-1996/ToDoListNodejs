6eab5d12b5227ff33d66db4b32aff95a22705f1d
emil-1996 <55382634+emil-1996@users.noreply.github.com>
2020-11-04 13:12:19 +0100

    Dodanie nasłuchiwania zdarzeń, kasowania elementów, renderowanie drzewa z bazy danych
    * hide form
    
    * edit style
    
    * Dodanie nasłuchiwania na zdarzenie oraz dodanie listy zadan na sztywno na razie
    
    * format kodu dodanie pobierania zadan z bazy
    
    * Podglad pojedynczego zadania
    
    * renderowanie html
    
    * Pobieranie zadan z bazy
    
    * po dodaniu zadania przeładuj aktualne zadania
    
    * Kasowanie elementów z bazy danych
    
    Co-authored-by: emil <emdom4@gmail.com>

--- myapp/views/js/main.js
+++ myapp/views/js/main.js
@@ -1,4 +1,14 @@
+function showForm() {
+    const modal = document.querySelector("#todo-app-form");
+    const btn = document.querySelector("span#close");
+    modal.style.display = "block";
+}
 
+function hideForm() {
+    const modal = document.querySelector("#todo-app-form");
+    const btn = document.querySelector("span#close");
+    modal.style.display = "none";
+}
 
 function getTaskFromForm() {
     const formTask = document.forms;
@@ -18,13 +28,80 @@ async function sendTask() {
             body: task
         });
         const result = await response.json();
-        if(result.error){
+        if (result.error) {
             throw JSON.parse(result.error);
         }
         alert(result.message);
-        console.log(result);
+        renderTasks();
     } catch (error) {
         console.log(error);
         alert(error.error);
     }
+}
+
+function removeElement(element) {
+    const idTask = element.querySelector('.element-id').innerHTML;
+    removeElementFromDB(idTask);
+    element.remove();
+}
+
+async function removeElementFromDB(idTask) {
+    try {
+        const response = await fetch("http://0.0.0.0:3000/delete", {
+        method: 'POST',
+        body: JSON.stringify({"_id": idTask})});
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    todoList.addEventListener("click", e => {
+        if (e.target.classList.contains("element-delete")) {
+            removeElement(e.target.parentElement.parentElement);
+        }
+    });
+});
+
+window.addEventListener('load', () => {
+    renderTasks();
+  });
+
+async function getData() {
+    try {
+        const response = await fetch("http://0.0.0.0:3000/get", {});
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderTasks() {
+    try {
+        let html = '';
+        const tasks = await getData();
+        for (let task of tasks) {
+           html += renderTask(task);
+        }
+        document.querySelector('#todoList.list').innerHTML = html;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function renderTask(task){
+    let output = '';
+    output += `<div class="element">
+    <div class="element-bar">
+        <h3 class="element-title">${task.name}</h3><button class="element-delete" title="Usuń task"></button></div>
+    <div class="element-details">
+        <h3 class="element-priority">${task.priority}</h3>
+        <h3 class="element-id">${task._id}</h3>
+    </div>
+    <div class="element-text"></div>${task.desc}</div>
+    </div>`
+    return output;
 }
